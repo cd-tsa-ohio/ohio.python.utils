9e6674e22fb78f52370f33bc5930acb71557dbff
sormaz <sormaz@ohio.edu>
2024-12-25 14:07:45 -0500

    changed version to 1.0.3

--- ou_utils/setup.py
+++ ou_utils/setup.py
@@ -10,6 +10,8 @@ setup(
     name='ou_file_utils',
     license='GPLv3',
     author="Dusan Sormaz",
-    author_email='sormaz@ohio.edu'
+    author_email='sormaz@ohio.edu',
+    url='https://github.com/cd-tsa-ohio/ohio.python.utils',
+    version='1.0.3'
     )
     
