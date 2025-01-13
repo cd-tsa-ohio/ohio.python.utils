fe8edf05fe4a6c30f4e252ebed5b667fae7913f7
sormaz <sormaz@ohio.edu>
2025-01-12 20:21:39 -0500

    skeleton of runnigi code for table generation - has errors

--- Milad-thesis-work/run-code-file.py
+++ Milad-thesis-work/run-code-file.py
@@ -4,6 +4,30 @@ import ou_file_utils as ou
 #for df in dfs:
 #   print (df.head())
 
+
+def makeSimioTables (select_func = feature_selection_max):
+
+    fs_drs = ou.getFilesDataFrames()
+    resFrame = pd.DataFrame
+    taskFrame = pd.DataFrame
+
+    for k in fs_drs.keys():
+        df = fs_drs[k]
+        entity = getEntity(k)
+        feat_process = select_func(df)
+        #resources
+     #   resFrame.append(getResources(feature_process))
+        # tasks
+        # processes
+        # routing
+        # arrivals
+        # parttable
+
+# file.rfind('/')
+#39
+# file[file.rfind('/'):]
+
+
 from feature_selection_module import feature_selection_max, feature_selection_min, feature_selection_median #import all three functions from the feature_selection_module
 
 # Load DataFrames using ou_file_utils
